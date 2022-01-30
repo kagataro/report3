a6e86ccb806e6b370eec86066aedd06e5cfc5953
Taro Kaga <kagataro@kagatarounoMacBook-Air.local>
2022-01-30 14:48:41 +0900

    Change Enemy.java

--- app/src/main/java/report3/Enemy.java
+++ app/src/main/java/report3/Enemy.java
@@ -51,5 +51,61 @@ public class Enemy {
             System.out.printf("モンスター%sは倒れた。\n", name);
         }
     }
+    /**
+     * 名前を取得するゲッター
+     * @return　Enemyの名前
+     */
+    public String getName(){
+        return name;
+    }
+    /**
+     * HPを取得するゲッター
+     * @return　EnemyのHP
+     */
+    public int getHitPoint(){
+        return hitPoint;
+    }
+    /**
+     * 攻撃値を取得するゲッター
+     * @return　Enemyの攻撃値
+     */
+    public int getAttack(){
+        return attack;
+    }
+    /**
+     * 生存状態を取得するゲッター
+     * @return　生存状態
+     */
+    public boolean getDead(){
+        return dead;
+    }
+    /**
+     * 名前を設定するセッター
+     * @param name 設定したい名前
+     */
+    public void setName(String name){
+        this.name = name;
+    }
+    /**
+     * HPを設定するセッター
+     * @param hitPoint 設定したいHP
+     */
+    public void setHitPoint(int hitPoint){
+        this.hitPoint = hitPoint;
+    }
+    /**
+     * 攻撃値を設定するセッター
+     * @param attack　設定したい攻撃値
+     */
+    public void setAttack(int attack){
+        this.attack = attack;
+    }
+    /**
+     * 生存状態を設定するセッター
+     * @param dead 設定したい生存状態
+     */
+    public void setDead(boolean dead){
+        this.dead = dead;
+    }
 
 }
